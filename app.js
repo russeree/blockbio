6cad38f480ab38f5865d93a30c7f7d220400dfeb
russeree <reese.russell@ymail.com>
2022-06-25 20:28:23 -0700

    waiting on essentials plus api access for twitter

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import dotenv from 'dotenv';
-import TwitterApi from 'twitter-api-v2';
+import {TwitterApi} from 'twitter-api-v2';
+import esMain from 'es-main'
 
 dotenv.config();
 
 class BlockBio{
     constructor(){
-
+        this.client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
     }
+    async getBio(){
+        console.log(await this.client.v2.me())
+    }
+}
+
+if(esMain(import.meta)){
+    console.log(`Running BlockBio as 'main()' - Enjoy`)
+    let blockBio = new BlockBio();
+    blockBio.getBio();
 }
