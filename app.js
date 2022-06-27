2fc7912da5838cb70862fc7a114e7773340931b2
russeree <reese.russell@ymail.com>
2022-06-27 00:14:13 -0700

    systemd template

--- app.js
+++ app.js
@@ -22,9 +22,11 @@ class BlockBio{
         });
         for(let ticker of geckoData.data.tickers){
             if(ticker.target == 'USD'){
+                let current = new Date();
+                let cTimeStamp = `${current.toLocaleDateString()} ${current.toLocaleTimeString()}`;
                 let priceText = parseInt(ticker.last).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                 console.log(`Updated bio price with a value of $${priceText}`)
-                await this.client.v1.updateAccountProfile({description: `${this.bioText} - Last BTC/USD $${priceText}`});
+                await this.client.v1.updateAccountProfile({description: `${this.bioText}last BTC/USD $${priceText} - ${cTimeStamp} PST`});
             }
         }
     }
@@ -33,16 +35,16 @@ class BlockBio{
 (async()=>{
     if(esMain(import.meta)){
         console.log(`Running BlockBio as 'main()' - Enjoy`)
-        let blockBio = new BlockBio('#bitcoin - custom bitcoin core .23 node operator and contributor - programming - self taught maker and investor - no price talk');
+        let blockBio = new BlockBio('#bitcoin - custom bitcoin core .23 node operator and contributor - programming - self taught maker - ');
         for(;;){
             try{
                 await blockBio.setPriceBio();
-                await new Promise(resolve => setTimeout(resolve, 60000));
             }
             catch{e=>{
                 console.log(`Failed to write Twitter profile description with error: ${e}`);
             }}
             finally{
+                await new Promise(resolve => setTimeout(resolve, 180000));
             }
         }
     }
